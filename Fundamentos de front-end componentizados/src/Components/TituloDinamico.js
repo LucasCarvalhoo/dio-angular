d98314e2b8442faf4ac8688edc122fd8b13ba11c
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-07-03 19:50:26 -0300

    estilizando meu component

--- Fundamentos de front-end componentizados/src/Components/TituloDinamico.js
+++ Fundamentos de front-end componentizados/src/Components/TituloDinamico.js
@@ -1,9 +1,24 @@
 class TituloDinamico extends HTMLElement{
-    construtor(){
+    constructor(){
         super();
 
-        const shadow = this.attachShadow({mode: "open"});
+        const shadow = this.attachShadow({mode: "open"}); // 
 
+        // base do component h1
+        const componentRoot = document.createElement("h1"); // criei o meu elemento
+        componentRoot.textContent = "My News"; // conteudo do meu elemento
+
+        // estilizar o component
+        const style = document.createElement("style"); // criei uma tag style pra estilizar o meu component
+
+        style.textContent = `
+            h1{
+                color: red;
+            }
+        `
+        // enviar para a shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
 
     }
 }
