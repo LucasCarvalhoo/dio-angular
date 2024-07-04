bebf4ee63bf372eaa0f7da28e36b1109b945a4e4
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-07-03 22:33:27 -0300

    usando dom para criar um component

--- Fundamentos de front-end componentizados/src/Components/CardNews.js
+++ Fundamentos de front-end componentizados/src/Components/CardNews.js
@@ -3,12 +3,34 @@ class Cardnews extends HTMLElement{
         super();
 
         const shadow = this.attachShadow({mode: "open"});
-
-        shadow.innerHTML = "<h1>hello</h1>"
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.style());
     }
-
+    // vai construir o meu elemento, a minha div
     build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card")
 
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card_left")
+        const span = document.createElement("span");
+        const h1 = document.createElement("h1");
+        const p = document.createElement("p");
+
+        cardLeft.appendChild(span);
+        cardLeft.appendChild(h1);
+        cardLeft.appendChild(p);
+
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card_right")
+        const img = document.createElement("img");
+        cardRight.appendChild(img);
+        img.setAttribute("src", "../assets/asset-vader.png")
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
     }
 
     styles(){
